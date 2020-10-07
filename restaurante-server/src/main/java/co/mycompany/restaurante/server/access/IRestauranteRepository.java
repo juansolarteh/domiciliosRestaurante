b64d0cff67ef6770cec4008e71bdc5538be70168
juansolarteh <juansolarteh@unicauca.edu.co>
2020-10-06 20:38:14 -0500

    config

---
+++ restaurante-server/src/main/java/co/mycompany/restaurante/server/access/IRestauranteRepository.java
@@ -0,0 +1,20 @@
+package co.mycompany.restaurante.server.access;
+
+import java.util.ArrayList;
+import co.mycompany.restaurante.commons.domain.Restaurante;
+import co.mycompany.restaurante.commons.domain.Plato;
+
+/**
+ * Interface del respositorio de clientes
+ * @author Libardo Pantoja
+ */
+public interface IRestauranteRepository {
+    /**
+     * Busca un Customer por su ceduka
+     * @param id cedula del cliente
+     * @return  objeto de tipo Customer
+     */
+    public String addMenuSemanal(ArrayList<Plato> menuSemanal);
+    
+    
+}
---
+++ restaurante-server/src/main/java/co/mycompany/restaurante/server/domain/services/RestauranteService.java
@@ -0,0 +1,43 @@
+package co.mycompany.restaurante.server.domain.services;
+
+import co.mycompany.restaurante.commons.domain.Restaurante;
+import co.mycompany.restaurante.commons.infra.JsonError;
+import co.mycompany.restaurante.commons.infra.Utilities;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import co.mycompany.restaurante.commons.domain.Plato;
+import co.mycompany.restaurante.server.access.IRestauranteRepository;
+
+/**
+ * Servicio de clientes. Da acceso a la lógica de negocio
+ *
+ * @author Libardo, Julio
+ */
+public class RestauranteService {
+
+    /**
+     * Repositorio de clientes
+     */
+    IRestauranteRepository repo;
+
+    /**
+     * Constructor parametrizado. Hace inyeccion de dependencias
+     *
+     * @param repo repositorio de tipo IRestauranteRepository
+     */
+    public RestauranteService(IRestauranteRepository repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * Buscar un cliente
+     *
+     * @param id cedula
+     * @return objeto tipo Restaurante
+     */
+    public String addMenuSemanal(ArrayList<Plato> menuSemanal) {
+        return repo.addMenuSemanal(menuSemanal);
+    }
+
+}
